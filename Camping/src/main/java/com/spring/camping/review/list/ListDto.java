4d9523764cc5fc181975df941c633d578d65ef70
RiaSong <donna_s@sookmyung.ac.kr>
2021-01-22 14:37:42 +0900

    1/21 _ Ria _ reviewList_ 중간커밋
    소스트리로 갈거야!@!!!!!!!

--- Camping/src/main/java/com/spring/camping/review/list/ListDto.java
+++ Camping/src/main/java/com/spring/camping/review/list/ListDto.java
@@ -28,13 +28,94 @@ public class ListDto {
 		this.reviewContent = reviewContent;
 		this.reviewDate = reviewDate;
 	}
+
+	public ListDto(int reviewNo, int userNo, int pNo, int orderNo, String userId, String reviewFile,
+			String reviewSubFile, String reviewContent, String reviewDate) {
+		super();
+		this.reviewNo = reviewNo;
+		this.userNo = userNo;
+		this.pNo = pNo;
+		this.orderNo = orderNo;
+		this.userId = userId;
+		this.reviewFile = reviewFile;
+		this.reviewSubFile = reviewSubFile;
+		this.reviewContent = reviewContent;
+		this.reviewDate = reviewDate;
+	}
+
+	// getter setter
+	public int getReviewNo() {
+		return reviewNo;
+	}
+
+	public void setReviewNo(int reviewNo) {
+		this.reviewNo = reviewNo;
+	}
+
+	public int getUserNo() {
+		return userNo;
+	}
+
+	public void setUserNo(int userNo) {
+		this.userNo = userNo;
+	}
+
+	public int getpNo() {
+		return pNo;
+	}
+
+	public void setpNo(int pNo) {
+		this.pNo = pNo;
+	}
+
+	public int getOrderNo() {
+		return orderNo;
+	}
+
+	public void setOrderNo(int orderNo) {
+		this.orderNo = orderNo;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getReviewFile() {
+		return reviewFile;
+	}
+
+	public void setReviewFile(String reviewFile) {
+		this.reviewFile = reviewFile;
+	}
+
+	public String getReviewSubFile() {
+		return reviewSubFile;
+	}
+
+	public void setReviewSubFile(String reviewSubFile) {
+		this.reviewSubFile = reviewSubFile;
+	}
+
+	public String getReviewContent() {
+		return reviewContent;
+	}
+
+	public void setReviewContent(String reviewContent) {
+		this.reviewContent = reviewContent;
+	}
+
+	public String getReviewDate() {
+		return reviewDate;
+	}
+
+	public void setReviewDate(String reviewDate) {
+		this.reviewDate = reviewDate;
+	}
 	
-	//
-	
-	
-	
-	
-	
-	
+
 	
 } // END
