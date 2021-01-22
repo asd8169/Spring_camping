ea6cdd25f3736c593de7a3276b731edd9b774f4f
jonghancha <jonghancha@gmail.com>
2021-01-22 15:56:10 +0900

    ICommand 업로드

---
+++ Camping/src/main/java/com/spring/camping/icommand/ICommand.java
@@ -0,0 +1,22 @@
+package com.spring.camping.icommand;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.ui.Model;
+
+
+//import com.mysql.cj.Session;
+
+public interface ICommand {
+
+	public void execute(SqlSession sqlSession, Model model, HttpSession session) throws ServletException, IOException;
+	
+
+	
+}
