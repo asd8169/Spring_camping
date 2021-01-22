b44bd1f641cad54da7475858fbab6eca3747457e
RiaSong <donna_s@sookmyung.ac.kr>
2021-01-22 18:14:28 +0900

    1/21 _ Ria _ 중간 커밋
    풀을 위한 커밋

---
+++ Camping/src/main/java/com/spring/camping/admin/product/insert/IDao.java
@@ -0,0 +1,9 @@
+package com.spring.camping.admin.product.insert;
+
+
+
+public interface IDao {
+
+	public void writeDao(String userNo, String reviewNo, String orderNo, String productNo, String reviewContent, String reviewFile, String reviewDate);
+	
+}
---
+++ Camping/src/main/java/com/spring/camping/admin/product/insert/RegisterProductController.java
@@ -0,0 +1,57 @@
+package com.spring.camping.admin.product.insert;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.spring.camping.fileupload.ProductFileUploadCommand;
+import com.spring.camping.icommand.ICommand;
+
+@Controller
+@RequestMapping("admin/")
+public class RegisterProductController {
+	
+	//xml과 java연결
+	@Autowired
+	private SqlSession sqlSession;
+	
+	ICommand command = null;
+//
+//	HttpServletRequest request;
+//	HttpSession session =request.getSession();
+//	
+//	// 상품 등록 폼 
+	@RequestMapping("product_write")
+	public String WriteForm() {
+		
+		return "admin/product_write";
+	}
+	
+	
+	// 상품 등록
+	@RequestMapping("register")
+	public String RegisterProduct(HttpServletRequest request, Model model, HttpSession session) throws ServletException, IOException { {
+
+		session = request.getSession();
+		
+		command = new ProductFileUploadCommand();
+		command.execute(sqlSession, request, model, session);
+		
+		
+		
+		
+		return "redirect:admin/admin_product_list";
+		}
+	}
+	
+	
+	
+} // END ---------------------------------------------------
